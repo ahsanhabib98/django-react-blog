fa7772f851b84c4fc78fa2c44e908b4a8706c411
Ahsan Habib <ahredoan@gmail.com>
2019-11-07 18:31:38 +0600

    update backend api

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,19 +1,41 @@
-from rest_framework.generics import \
-    (ListAPIView,
-     CreateAPIView,
-     RetrieveAPIView,
-     UpdateAPIView
-     )
-
 from articles.models import Article
 from .serializers import ArticleSerializer
+from rest_framework import viewsets
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
-
-
-class ArticDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+
+# from rest_framework.generics import \
+#     (ListAPIView,
+#      CreateAPIView,
+#      RetrieveAPIView,
+#      UpdateAPIView,
+#      DestroyAPIView
+#      )
+
+
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleUpdateView(UpdateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDeleteView(DestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
