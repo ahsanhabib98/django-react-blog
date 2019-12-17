12ac333b9bfc71af9433dabc84713b682d8d9b8f
Ahsan Habib <ahredoan@gmail.com>
2019-12-17 15:21:03 +0600

    updated

--- djreact/settings.py
+++ djreact/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "build/static"),
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
