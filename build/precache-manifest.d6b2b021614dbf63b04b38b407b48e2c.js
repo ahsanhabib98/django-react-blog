96e1bb794c9c65b53cf409fabdb7d0b780d6d814
ahsanhabib98 <ahredoan@gmail.com>
2020-03-21 13:06:04 +0600

    updated

---
+++ build/precache-manifest.d6b2b021614dbf63b04b38b407b48e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94b6e454b6b3acccdffc44b3ca5854c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f581fbba272cc8d3351c",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "f581fbba272cc8d3351c",
+    "url": "/static/js/2.93c5224a.chunk.js"
+  },
+  {
+    "revision": "a81e893c8aa445d8ef84",
+    "url": "/static/js/main.094671dc.chunk.js"
+  },
+  {
+    "revision": "60f05623933eb017ae52",
+    "url": "/static/js/runtime-main.78c084b0.js"
+  }
+]);
